c1ec02cdbb6482c17bfc1ae314934d8eee1919cc
jigyasaG18 <158419387+jigyasaG18@users.noreply.github.com>
2024-12-06 19:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,15 @@ import streamlit as st
 import requests
 import os
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data.get('poster_path')
-    if poster_path is not None:
-        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-        return full_path
-    return None
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names, recommended_movie_posters
-
 def download_file_from_google_drive(file_id, destination):
-    URL = f"https://drive.google.com/uc?id={file_id}"  # Updated URL format for direct download
+    URL = f"https://drive.google.com/uc?id={file_id}"  # Correct the download URL
     response = requests.get(URL)
-    with open(destination, 'wb') as f:
-        f.write(response.content)
+    
+    if response.status_code == 200:
+        with open(destination, 'wb') as f:
+            f.write(response.content)
+    else:
+        st.error("Failed to download file. Please check the Google Drive link or your internet connection.")
 
 st.header('Movie Recommender System')
 
@@ -49,7 +31,11 @@ if not os.path.exists(similarity_file_path):
 else:
     st.write("Found similarity.pkl, loading it...")
 
-similarity = pickle.load(open(similarity_file_path, 'rb'))
+# Attempt to load the similarity matrix
+try:
+    similarity = pickle.load(open(similarity_file_path, 'rb'))
+except Exception as e:
+    st.error(f"Error loading similarity matrix: {e}")
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -72,4 +58,4 @@ if st.button('Show Recommendation'):
 
 # Google Drive URL construction for other purposes
 share_url = f'https://drive.google.com/file/d/{similarity_file_id}/view?usp=sharing'
-st.markdown(f"Access the file via this [Google Drive link]({share_url})")
+st.markdown(f"Access the file via this [Google Drive link]({share_url})")s
